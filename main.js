087f84b24ebc4ad2b6768101c847a4e17ca2df3d
rijalislamir <rijalislamir@gmail.com>
2023-02-14 22:02:24 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: '0.0.0.0'
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/user',
+      handler: (request, h) => {
+          return 'get user';
+      }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
