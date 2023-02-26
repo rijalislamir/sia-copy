da0eab03c2deec793cb3a586dabcb8c3245e1b4f
rijalislamir <rijalislamir@gmail.com>
2023-02-26 16:48:34 +0700

    change port and host

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0'
+        port: 5000,
+        host: 'localhost'
     });
 
     server.route({
